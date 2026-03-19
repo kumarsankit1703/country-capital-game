22b377eccae50f73d1261623772f5c0a437db6b6
Sankit Kumar <sankitkumar1703@gmail.com>
2026-03-20 01:24:23 +0530

    Adding Comments for Help

--- src/Game.js
+++ src/Game.js
@@ -10,16 +10,18 @@ const Game = ({ data }) => {
   const [isMatched, setIsMatched] = useState([]);
   const [wrong, setWrong] = useState([]);
 
-   const handleClick = (e, option) => {
-    if (selectedOption.length === 2) return;
-    if (selectedOption.includes(option)) return;
+  const handleClick = (e, option) => {
+    if (selectedOption.length === 2) return; // if Array length is more than 2 ['India', 'Delhi']
+    if (selectedOption.includes(option)) return; // if value is already existing in selectedOption ["Delhi"] 
+                                                //(Agar selected value pehle se hi added hai selectedOption mein to wo array mein add nahi hogi)
     setSelectedOption((prev) => [...prev, option]);
   };
 
   useEffect(() => {
-    if (selectedOption.length === 2) {
-      const [first, second] = selectedOption;
-      const match = data[first] === second || data[second] === first;
+    if (selectedOption.length === 2) {                                        // Agar Array ki length 2 hai
+      const [first, second] = selectedOption;                                 // Destructure array values ['India', 'Delhi'] = selectedOption
+      console.log(first, second, data[first], data[second])
+      const match = data[first] === second || data[second] === first;         // data[Delhi] === India || data[India] === Delhi
 
       if (match) {
         setIsMatched((prev) => [...prev, first, second]);
@@ -33,7 +35,6 @@ const Game = ({ data }) => {
       }, 1000);
     }
   }, [selectedOption]);
- 
 
   if (isMatched.length === item.length) {
     return <h1>Congratulation</h1>;
